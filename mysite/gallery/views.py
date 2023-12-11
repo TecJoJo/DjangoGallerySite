bfa687dcde1228b9b96f4123ca945235535b56ac
Yao Lu Home Win11 <luyao2493751@gmail.com>
2023-12-11 17:59:41 +0200

    thumbnails work

--- mysite/gallery/views.py
+++ mysite/gallery/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render,redirect
 from gallery.models import Post
 from django.contrib.auth.models import User
 from gallery.forms import UploadForm
+from PIL import Image
+import os
+
+
 
 def display_images(request):
     if request.method == 'GET':
@@ -10,34 +14,65 @@ def display_images(request):
         return render(request, 'gallery/index.html', {'user_posts' : posts_of_user,"rest_posts":posts_of_rest_users})
 
     
-from django.contrib.auth.decorators import login_required
+
+
+
 
 
 def image_upload(request):
+    
+    size = (300, 300)
+
     if request.method == 'POST':
-        # Create a mutable copy of Post
+        # Get the uploaded image file
+        img_file = request.FILES["image"]
+
+       
         mutable_post = request.POST.copy()
-
-        
         current_user_id = request.user.id
-
-        
-        mutable_post["user"] = str(current_user_id)
-
         form = UploadForm(mutable_post, request.FILES)
+
         if form.is_valid():
-            # Create a form instance without saving it to the database
+            
             post_instance = form.save(commit=False)
 
-            # Set the user_id field before saving
+            
             post_instance.user_id = current_user_id
             post_instance.save()
+
+            
+            img_id = post_instance.id
+
+            # Open the uploaded image using PIL
+            with Image.open(img_file) as im:
+                # Convert the image to RGB mode
+                im = im.convert("RGB")
+
+                # Resize the image to exactly 300x300
+                im = im.resize(size)
+
+                # Specify the directory to save the thumbnail
+                thumbnail_dir = os.path.join("media", "thumbnails")
+
+                # Create the directory if it doesn't exist
+                os.makedirs(thumbnail_dir, exist_ok=True)
+
+                # Specify the name for the thumbnail using the image ID
+                thumbnail_name = f"{img_id}_thumbnail.jpg"
+
+                # Specify the full path to save the thumbnail
+                thumbnail_path = os.path.join(thumbnail_dir, thumbnail_name)
+
+                
+                im.save(thumbnail_path, "JPEG")
+                
 
             return redirect('gallery:success')
     else:
         form = UploadForm()
 
     return render(request, 'gallery/upload.html', {'form': form})
+
 
 
 def image_detail(request,pk):
